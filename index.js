f67587a9d56617a9ff954f2e0e16326ea7b951dc
Stefan Grönke <stefan@gronke.net>
2015-11-22 19:12:33 +0100

    specify api base_dir manually

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var path = require('path');
 var express = require('express');
 var app = express();
 
 var port = process.env['PORT'] || 3000;
 
 var Route = require('./lib/Route');
-var route = new Route('/api/v1', app);
+var route = new Route('/api/v1', app, {
+	baseDir: path.resolve(__dirname)
+});
 
 app.listen(port);
