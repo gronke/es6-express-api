88eb5c56022600b42cb3cd7fa01d71cf1619c426
Stefan Grönke <stefan@gronke.net>
2015-11-22 19:27:48 +0100

    export Route

---
+++ standalone.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var host = "0.0.0.0";
+var port = process.env['PORT'] || 3000;
+
+var Route = require('./lib/Route');
+var route = new Route('/api/v1', app, {
+	baseDir: path.resolve(__dirname)
+});
+
+var server = app.listen(port, host, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('listening at http://%s:%s', host, port);
+});
